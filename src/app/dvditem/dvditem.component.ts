da16bc71331df7d4b7c59a620f2179a2fe66c84d
minahuang <68625630+minahuang@users.noreply.github.com>
2020-08-24 10:16:03 +0800

    更新版

--- src/app/dvditem/dvditem.component.ts
+++ src/app/dvditem/dvditem.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { Dvditem } from '../dvditem';
 import {DvditemService} from '../dvditem.service'
 import { Observable, Subject } from 'rxjs';
 @Component({
   selector: 'app-dvditem',
   templateUrl: './dvditem.component.html',
-  styleUrls: ['./dvditem.component.css']
+  styleUrls: ['./dvditem.component.css'],
+  providers: [DatePipe]
 })
 export class DvditemComponent implements OnInit {
   displayedColumns: string[] = ['id', 'title', 'date', 'genre', 'dsc'];
   dvditems$: Observable<any>;
   dvditem$: Observable<Dvditem>;
   selectedDvditem:Dvditem;
+  json:string;
+  @Input() getDataFromChild;
   constructor(
     private dvditemService:DvditemService,
+    private datePipe : DatePipe
   ) { }
 
 
@@ -34,11 +39,17 @@ export class DvditemComponent implements OnInit {
 
   }
   getDvditems(): void {
-    this.dvditems$ =this.dvditemService.getDvditems();
+  //   this.dvditemService.getDvditems().subscribe(els => {
+  //     this.dvditems= els as Dvditem[]
+  // });
+  this.dvditems$=this.dvditemService.getDvditems();
+    
     
   }
 
   createDvditem(title:string,date:string,genre:string,dsc:string):void{
+   
+    date =this.datePipe.transform(date, 'MM-dd-yyyy');
     title = title.trim();
     date = date.trim();
     genre = genre.trim();
@@ -46,6 +57,14 @@ export class DvditemComponent implements OnInit {
     if (!title || !date || !genre|| !dsc) { return; }
     this.dvditemService.createDvditem({ title,date,genre,dsc } as Dvditem)
     .subscribe();
+    const info={
+      id:null,
+      title,
+      date,
+      genre,
+      dsc
+  } 
+    this.json=JSON.stringify(info);
     this.getDvditems();
     this.initDvditem();
   }
@@ -56,20 +75,23 @@ export class DvditemComponent implements OnInit {
     genre = genre.trim();
     dsc = dsc.trim();
     if ( !title || !date || !genre|| !dsc) { return; }
-    const item={
+    const info={
       id:this.selectedDvditem.id,
       title,
       date,
       genre,
       dsc
   }
-    this.dvditemService.updateDvditem(item)
+    this.dvditemService.updateDvditem(info)
     .subscribe(()=> this.getDvditems());
+    this.json=JSON.stringify(info);
     this.initDvditem();
   }
 
   delete(number:Number): void {
     this.dvditemService.deleteDvditem(number.toString()).subscribe();
+    const info={id:number}
+    this.json=JSON.stringify(info);
     this.getDvditems();
     this.initDvditem();
   }
